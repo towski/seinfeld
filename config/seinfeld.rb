06ab5b62df05decd5790236c06e2d6bfe5a98b55
rick <technoweenie@gmail.com>
2009-03-08 12:46:47 -0700

    update config

---
+++ config/seinfeld.rb
@@ -0,0 +1,4 @@
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+Seinfeld::User.github_login    = 'calendaraboutnothing'
+Seinfeld::User.github_password = 'jGrhHd8sBQsffaDNjwroMftDZ2MYRK'
+Seinfeld::User.creation_token  = 'BgDAdmtYbjks6N3ycNxgHpiyrUpLps'
