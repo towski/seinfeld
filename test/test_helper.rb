d1925c6e6a3740cd0d43fa5cc774cdf64e5543dc
rick <technoweenie@gmail.com>
2010-05-02 08:52:02 -0700

    remove vendored libs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,14 @@ require 'yaml'
 
 RunningMan.setup_on ActiveSupport::TestCase, :ActiveRecordBlock
 
+class ActiveSupport::TestCase
+  FEED_PATH = File.join(File.dirname(__FILE__), 'feeds')
+
+  def feed_data(feed)
+    IO.read File.join(FEED_PATH, "#{feed}.atom")
+  end
+end
+
 begin
   require 'ruby-debug'
   Debugger.start
