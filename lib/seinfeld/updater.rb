2b6cfa787fdb8cc11889c724603d0e7754460e83
rick <technoweenie@gmail.com>
2010-05-08 22:27:22 -0700

    take out useless debug messages

--- lib/seinfeld/updater.rb
+++ lib/seinfeld/updater.rb
@@ -13,10 +13,8 @@ class Seinfeld
     def run(today = Date.today)
       Time.zone = @user.time_zone || "UTC"
       if feed = Feed.fetch(@user.login)
-        puts "#{@user.login}: #{feed.inspect} #{feed.committed_days.inspect}"
         @user.update_progress(feed.committed_days, today)
       else
-        puts "#{@user.login}: wha wha whahhhh"
         @user.disabled = true
         @user.save!
       end
